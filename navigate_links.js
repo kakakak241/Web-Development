8731a88e013f5ee90dce17a71f05fb46ea8e95d6
kakakak241 <danilazubov31@gmail.com>
2022-11-18 20:16:46 +0300

    fix: change navigate_links.js

--- navigate_links.js
+++ navigate_links.js
@@ -1,22 +1,21 @@
 window.addEventListener("load", function () {
     let links = document.querySelectorAll("nav.menu a")
     console.log(links)
-    function activeLink(){
+    function currentLink(){
         let path = window.location.href;
         let currentElement = path.split('#');
+
+        console.log(currentElement)
+
         if(currentElement.length > 1) {
             currentElement = currentElement[1]
         }
 
-        for (let i = 0; i < links.length; ++i) {
-            console.log('#' + currentElement === links[i].hash)
-            if ('#' + currentElement === links[i].hash) {
-                links[i].classList.add("navigates")
-            } else {
-                links[i].classList.remove("navigates")
-            }
-        }
+        links.forEach((item) => {
+            console.log('#' + currentElement === item.hash)
+            '#' + currentElement === item.hash ? item.classList.add("navigates") : item.classList.remove("navigates")
+        })
     }
-    window.addEventListener("scroll", activeLink)
-    activeLink()
+    window.addEventListener("scroll", currentLink)
+    currentLink()
 })
